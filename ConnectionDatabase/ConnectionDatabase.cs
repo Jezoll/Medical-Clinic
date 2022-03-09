ac9daa102624f63f56bb9742401e283885b3a146
Hubert <n3tr00x99@gmail.com>
2022-03-09 19:23:58 +0100

    login form v1.0 - variable session

---
+++ ConnectionDatabase/ConnectionDatabase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConnectionDatabase
+{
+    public class Connection_Query
+    {
+        string ConnectionString = @"Password=!Testowanie; User ID=rdsxx; Data Source=medicalclinic.database.windows.net; Initial Catalog=MedicalClinic";
+        SqlConnection conn;
+
+        public void openConnection() //creates new sql connection
+        {
+            conn = new SqlConnection(ConnectionString);
+            conn.Open();
+        }
+
+        public void closeConnection() //close existing sql connection
+        {
+            conn.Close();
+        }
+
+        public SqlCommand executeQuery(string query) //execute queries 
+        {
+            SqlCommand cmd = new SqlCommand(query, conn);
+            return cmd;
+        }
+
+        public SqlDataReader dataReader(string query) //execute query and returns SqlDataReader object
+        {
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+        public SqlDataAdapter dataAdapter(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            return da;
+        }
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using ConnectionDatabase;
+
+namespace webapp
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IncorrectDataLabel.Visible = false;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {   
+                Connection_Query con = new Connection_Query();
+                // SqlConnection con = new SqlConnection(@"Password=!Testowanie; User ID=rdsxx; Data Source=medicalclinic.database.windows.net; Initial Catalog=MedicalClinic");
+
+                con.openConnection();
+                //con.Open();
+                //SqlCommand cmd = new SqlCommand("select * from tb_login where login_u = @username and password_u = @password COLLATE Polish_CS_AI", con);
+                SqlCommand cmd = con.executeQuery("select * from tb_login where login_u = @username and password_u = @password COLLATE Polish_CS_AI");
+
+                cmd.Parameters.AddWithValue("@username", TextBox1.Text);
+                cmd.Parameters.AddWithValue("@password", TextBox2.Text);
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                //int i = cmd.ExecuteNonQuery();
+                if(dt.Rows.Count > 0)
+                {
+                    Session["id"] = TextBox1.Text;
+                    Response.Redirect("Dashboard.aspx");
+                    Session.RemoveAll();
+                }
+                else
+                {
+                    IncorrectDataLabel.Visible = true;
+                }
+                //con.Close();
+                con.closeConnection();
+            }
+            catch
+            {
+                IncorrectDataLabel.Text = "Błąd";
+            }
+        }     
+    }
+}
