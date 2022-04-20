673d7274cbfcb39dd6c811cf0d58b656d17e7dd4
Adam Kupisz <UL0253541@edu.uni.lodz.pl>
2022-04-20 22:03:36 +0200

    Added: class (Patients, Appointments),listing patients (filtering, sorting, details), adding new patients, deleting patients.

---
+++ medicalclinic_back/Appointments.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace medicalclinic_back
+{
+    public class Appointment
+    {
+        private int id;
+        private int duration;
+        private Boolean confirmed;
+        private string type;
+        private int id_employee;
+        private int id_patient;
+        private int id_office;
+        private DateTime date_of_appointment;
+
+        public int Id { get => id; }
+        public int Duration { get => duration; }
+        public Boolean Confirmed { get => confirmed; }
+        public string Type { get => type; }
+        public int Id_employee { get => id_employee; }
+        public int Id_patient { get => id_patient; }
+        public int Id_office { get => id_office; }
+        public DateTime Date_of_appointment { get => date_of_appointment; }
+
+        public Appointment(int id, int duration, Boolean confirmed, string type, int id_employee, int id_patient, int id_office, DateTime date_of_appointment)
+        {
+            this.id = id;
+            this.duration = duration;
+            this.confirmed = confirmed;
+            this.type = type;
+            this.id_employee = id_employee;
+            this.id_patient = id_patient;
+            this.id_office = id_office;
+            this.date_of_appointment = date_of_appointment;
+        }
+
+        public static List<Appointment> GetAllAppointments(string sort_column = "visits.id", string sort_direction = "ASC")
+        {
+            Database.openConnection();
+            string query = $"SELECT visits.id, duration, confirmed, type, id_employee, id_patient, id_office, date FROM visits ORDER BY {sort_column} {sort_direction}";
+
+            MySqlDataReader data = Database.dataReader(query);
+
+            List<Appointment> appointments = new List<Appointment>();
+            while (data.Read())
+            {
+                Appointment appointment = new Appointment(data.GetInt32(0), data.GetInt32(1), data.GetBoolean(2), data.GetString(3), data.GetInt32(4), data.GetInt32(5), data.GetInt32(6), data.GetDateTime(7));
+
+                appointments.Add(appointment);
+            }
+
+            Database.closeConnection();
+            return appointments;
+        }
+
+
+    }
+}
---
+++ medicalclinic_front/PatientDetails.aspx.cs
@@ -0,0 +1,82 @@
+﻿using medicalclinic_back;
+using System;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+namespace medicalclinic
+{
+    public partial class PatientDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                int selected_patient_id = Int32.Parse(Request.QueryString["selected_patient_id"]);
+                string p_name = null;
+                string p_surname = null;
+                string p_pesel = null;
+                string last_apppointment_date = null;
+                List<Patient> patients = Patient.GetPatients(p_name, p_surname, p_pesel, last_apppointment_date);
+                Label_id_value.Text = selected_patient_id.ToString();
+                foreach (Patient p in patients)
+                {
+                    if (p.Id == selected_patient_id)
+                    {
+                        Label_first_name_value.Text = p.First_name;
+                        TextBox_first_name.Text = p.First_name.ToString();
+                        Label_surname_value.Text = p.Second_name;
+                        TextBox_surname.Text = p.Second_name.ToString();
+                        if (p.Sex.ToString() == "M")
+                        {
+                            Label_sex_value.Text = "Male";
+                            RadioButton_sex_male.Checked = true;
+                        }
+                        else
+                        {
+                            Label_sex_value.Text = "Female";
+                            RadioButton_sex_female.Checked = true;
+                        }
+                        Label_date_of_birth_value.Text = p.Date_of_birth.ToString("yyyy-MM-dd");
+                        TextBox_date_of_birth.Text = p.Date_of_birth.ToString("yyyy-MM-dd");
+                        Label_pesel_value.Text = p.Pesel;
+                        TextBox_pesel.Text = p.Pesel.ToString();
+                        Label_phone_number_value.Text = p.Phone_number;
+                        TextBox_phone_number.Text = p.Phone_number.ToString();
+                        Label_email_value.Text = p.Email;
+                        TextBox_email.Text = p.Email.ToString();
+                    }
+                }
+
+                List<Appointment> appointments = Appointment.GetAllAppointments("visits.id", "ASC");
+                List<Appointment> appointment_details = new List<Appointment>();
+                foreach (Appointment a in appointments)
+                {
+                    if (a.Id_patient == selected_patient_id)
+                    {
+                        appointment_details.Add(a);
+                    }
+
+                }
+                GridViewAppointments.DataSource = appointment_details;
+                GridViewAppointments.DataBind();
+            }
+        }
+
+        protected void Button_close_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ListPatients.aspx");
+        }
+
+        protected void Button_Delete_Click(object sender, EventArgs e)
+        {
+            int selected_patient_id = Int32.Parse(Request.QueryString["selected_patient_id"]);
+            string confirm_value = ConfirmMessageResponse.Value;
+            if (confirm_value == "Yes")
+            {
+                Patient.DeletePatient(selected_patient_id);
+                Response.Redirect("ListPatients.aspx");
+            }
+        }
+
+    }
+}
