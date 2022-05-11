f11dfc37b25a6c0f5bd4ea24ef7a48620dd164ff
Marek Tadeusiak <UL0253573@edu.uni.lodz.pl>
2022-05-11 20:09:41 +0200

    Added: EmailUnitTest

---
+++ medicalclinic_tests/EmailUnitTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using medicalclinic_back;
+using System;
+
+namespace medicalclinic_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void EmailValidateUnitTest()
+        {
+            string email = "emailunittest@test.pl";
+            Boolean expected_result = true;
+            Boolean actual_result = Patient.ValidateEmail(email);
+
+            Assert.AreEqual(expected_result, actual_result, "Wynik testu jednostkwoego nie jest zgodny z walidacj¹ maila");
+        }
+    }
+}
