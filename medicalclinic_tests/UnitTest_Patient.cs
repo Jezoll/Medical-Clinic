3dbab2e780a48e69041b569730e0a85d6bd22d2e
Wojciech Kurczyński <UL0253569@edu.uni.lodz.pl>
2022-05-11 20:35:02 +0200

    Added more unit tests

--- medicalclinic_tests/UnitTest_Patient.cs
+++ medicalclinic_tests/UnitTest_Patient.cs
@@ -10,13 +10,51 @@ namespace medicalclinic_tests
         public void Setup()
         {
         }
+        [Test]
+        public void NameUnitTest()
+        {
+            string name = "ㄆkasz";
+            Boolean expected_result = true;
+            Boolean actual_result = Patient.ValidateName(name);
 
+            Assert.AreEqual(expected_result, actual_result, "Wynik testu jednostkowego nie jest zgodny z za這瞠niami");
+        }
+        [Test]
+        public void SurnameUnitTest()
+        {
+            string surname = "Kowalski";
+            Boolean expected_result = true;
+            Boolean actual_result = Patient.ValidateName(surname);
+
+            Assert.AreEqual(expected_result, actual_result, "Wynik testu jednostkowego nie jest zgodny z za這瞠niami");
+        }
         [Test]
         public void PhoneNumberUnitTest()
         {
-            string phone_number = "6669991110";
+            string phone_number = "666999111";
             Boolean expected_result = true;
             Boolean actual_result = Patient.ValidatePhoneNumber(phone_number);
+
+            Assert.AreEqual(expected_result, actual_result, "Wynik testu jednostkowego nie jest zgodny z za這瞠niami");
+        }
+        [Test]
+        public void EmailValidateUnitTest()
+        {
+            string email = "emailunittest@test.pl";
+            Boolean expected_result = true;
+            Boolean actual_result = Patient.ValidateEmail(email);
+
+            Assert.AreEqual(expected_result, actual_result, "Wynik testu jednostkowego nie jest zgodny z za這瞠niami");
+        }
+        [Test]
+        public void PeselValidationUnitTest()
+        {
+            string pesel = "00010128675";
+            DateTime date = new DateTime(1900,01,01); //year,month,day
+            string sex = "M";
+
+            Boolean expected_result = true;
+            Boolean actual_result = Patient.ValidatePesel(pesel,date,sex);
 
             Assert.AreEqual(expected_result, actual_result, "Wynik testu jednostkowego nie jest zgodny z za這瞠niami");
         }
