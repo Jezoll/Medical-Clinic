43063593ea47bb8ddb2e2f10629411ff774d17e5
Hubert <n3tr00x99@gmail.com>
2022-05-26 08:49:22 +0200

    correct a unit tests

--- medicalclinic_tests/UnitTest1.cs
+++ medicalclinic_tests/UnitTest1.cs
@@ -10,38 +10,44 @@ namespace medicalclinic_tests
         [TestMethod]
         public void passwordValidationIsCorrect()
         {
-            var result = RecoveryPassword.passwordValidation("Test12345!");
-            Assert.IsTrue(result);
+            var result = RecoveryPassword.passwordValidation("Test1!233");
+            Assert.IsTrue(result, "Walidacja poprawna");
         }
         [TestMethod]
         public void tooShortPassword()
         {
-            var result = RecoveryPassword.passwordValidation("Test12!");
-            Assert.IsTrue(result);
+            var result = RecoveryPassword.passwordValidation("Test123");
+            Assert.IsFalse(result, "Hasło o odpowiedniej ilości znaków");
         }
         [TestMethod]
         public void passwordWithoutSpecialMark()
         {
             var result = RecoveryPassword.passwordValidation("Test12345");
-            Assert.IsTrue(result);
+            Assert.IsFalse(result, "Hasło posiada znak specjalny");
         }
         [TestMethod]
         public void passwordWithoutTheUpperCase()
         {
             var result = RecoveryPassword.passwordValidation("test12345!");
-            Assert.IsTrue(result);
+            Assert.IsFalse(result, "Hasło posiada dużą literę");
         }
         [TestMethod]
         public void PasswordInputsAreTheSame()
         {
             var result = RecoveryPassword.isPasswordTheSame("test", "test");
-            Assert.IsTrue(result);
+            Assert.IsTrue(result, "Pola haseł nie są takie same");
         }
         [TestMethod]
         public void oneInputIsDiffrentFromTheOther()
         {
             var result = RecoveryPassword.isPasswordTheSame("test", "test123");
-            Assert.IsTrue(result);
+            Assert.IsFalse(result, "Pola haseł są identyczne ");
+        }
+        [TestMethod]
+        public void passwordWithoutTheLowerCase()
+        {
+            var result = RecoveryPassword.passwordValidation("TEST123!");
+            Assert.IsFalse(result, "Wprowadzone hasło posiada małą literę");
         }
     }
 }
