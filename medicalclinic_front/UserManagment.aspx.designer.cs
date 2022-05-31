f7d22ad0b0e72ed6baa0f30251379d27b7b8f4f8
Hubert <n3tr00x99@gmail.com>
2022-05-31 17:17:57 +0200

    styling a table of users in the system

--- medicalclinic_front/UserManagment.aspx.designer.cs
+++ medicalclinic_front/UserManagment.aspx.designer.cs
@@ -22,14 +22,5 @@ namespace medicalclinic
         /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView UserTable;
-
-        /// <summary>
-        /// Kontrolka Button1.
-        /// </summary>
-        /// <remarks>
-        /// Pole generowane automatycznie.
-        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
